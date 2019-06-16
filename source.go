bf42ddba9749802a520066e66beffc9f909e98f6
Jere Käpyaho <jere@coniferproductions.com>
2019-06-17 00:21:04 +0300

    Added Source data definitions

--- source.go
+++ source.go
@@ -7,6 +7,49 @@ type Harmonic struct {
 
 }
 
+type HarmonicSelection uint8
+
+const (
+	Live HarmonicSelection = 0
+	Die  HarmonicSelection = 1
+	All  HarmonicSelection = 2
+)
+
+type HarmonicModulation struct {
+	IsOn           bool
+	EnvelopeNumber uint8
+}
+
+type HarmonicSettings struct {
+	VelocityDepth   int8  // 0~±31
+	PressureDepth   int8  // 0~±31
+	KeyScalingDepth int8  // 0~±31
+	LFODepth        uint8 // 0~31
+
+	EnvelopeSettings   [4]HarmonicEnvelopeSettings
+	IsModulationActive bool
+	Selection          HarmonicSelection
+
+	RangeFrom uint8 // 1~63
+	RangeTo   uint8 // 1~63
+
+	Odd    HarmonicModulation
+	Even   HarmonicModulation
+	Octave HarmonicModulation
+	Fifth  HarmonicModulation
+	All    HarmonicModulation
+
+	Angle  uint8 // 0/-, 1/0, 1/+ (maybe should be 2/+ ?)
+	Number uint8 // 1~63
+
+	IsShadowOn bool
+}
+
+type HarmonicEnvelopeSettings struct {
+	IsActive bool
+	Effect   uint8 // 0~31 (SysEx manual says "s<x> env<y> off", maybe should be "eff"?)
+}
+
 type EnvelopeSegment struct {
 	Rate  byte
 	Level byte
@@ -19,14 +62,68 @@ type Envelope struct {
 	IsLooping bool
 }
 
+type Filter struct {
+	IsActive              bool  // param 105
+	IsModulationActive    bool  // param 106
+	Cutoff                uint8 // 0~99
+	CutoffModulation      uint8 // 0~31
+	Slope                 uint8 // 0~31
+	SlopeModulation       uint8 // 0~31
+	FlatLevel             uint8 // 0~31
+	VelocityDepth         int8  // 0~±31
+	PressureDepth         int8  // 0~±31
+	KeyScalingDepth       int8  // 0~±31
+	EnvelopeDepth         int8  // 0~±31
+	VelocityEnvelopeDepth int8  // 0~±31
+	LFODepth              uint8 // 0~31
+}
+
+type KeyScaling struct {
+	Right      int8  // 0~±31
+	Left       int8  // 0~±31
+	Breakpoint uint8 // 0~127
+}
+
+type Amplifier struct {
+	IsActive            bool
+	AttackVelocityDepth int8  // 0~±31
+	PressureDepth       int8  // 0~±31
+	KeyScalingDepth     int8  // 0~±31
+	LFODepth            uint8 // 0~31
+	AttackVelocityRate  int8  // 0~±15
+	ReleaseVelocityRate int8  // 0~±15
+	KeyScalingRate      int8  // 0~±15
+	Envelope            Envelope
+}
+
 type Source struct {
 	// DFG
 	Coarse int8 // 0~±48
 	Fine   int8 // 0~±31
 
+	EnvelopeDepth         int8  // 0~±24
+	PressureDepth         int8  // 0~±31
+	BenderDepth           int8  // 0~±24  (or 0~24?)
+	VelocityEnvelopeDepth int8  // 0~±31
+	LFODepth              uint8 // 0~31
+	PressureLFODepth      int8  // 0~±31
+
+	Envelope Envelope
+
 	// DHG
 	Harmonics         [64]Harmonic
+	HarmonicSettings  HarmonicSettings
 	HarmonicEnvelopes [6]Envelope
+
+	// DDF (S381~S426)
+	Filter         Filter
+	FilterEnvelope Envelope
+
+	// KS
+	KeyScaling KeyScaling
+
+	// DDA
+	Amplifier Amplifier
 }
 
 type SourceSettings struct {
